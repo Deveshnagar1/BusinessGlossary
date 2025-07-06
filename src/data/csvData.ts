b1fc939b39e8b5ae08cb793f5c9525c8f886418a
Devesh Nagar <dkn.verse@gmail.com>
2025-07-07 01:11:03 +0530

    Fix data type for Gender Code in csvData and update formatting for consistency

--- src/data/csvData.ts
+++ src/data/csvData.ts
@@ -1,7 +1,7 @@
 export const csvData = `DOMAIN,LOGICAL TAB NAME,ATTRIBUTE NAME,PHYSICAL TABLE NAME,TABLE NAME,DEFINITION,DATA TYPE
-Member,Member Address Dimension,Latitude Number,GIS_LAT,TABLE_ADR,The geographic latitude coordinate of the member's address,Decimal(11,6)
-Member,Member Address Dimension,Longitude Number,GIS_LONG,TABLE_ADR,The geographic longitude coordinate of the member's address,Decimal(11,6)
-Member,Member Demographic Dimension,Gender Code,CDE_GNDR,TABLE_DEMO,A code representing gender of the member, used for demographic analysis,Varchar(10)
+Member,Member Address Dimension,Latitude Number,GIS_LAT,TABLE_ADR,The geographic latitude coordinate of the member's address,"Decimal(11,6)"
+Member,Member Address Dimension,Longitude Number,GIS_LONG,TABLE_ADR,The geographic longitude coordinate of the member's address,"Decimal(11,6)"
+Member,Member Demographic Dimension,Gender Code,CDE_GNDR,TABLE_DEMO,A code representing gender of the member,Varchar(5)
 Provider,Provider Enrollment Dimension,Contract Description,PROC_CNTRCT,TABLE_ENROLL,Description of Provider Contract,Varchar(50)
 Provider,Provider License Fact,License Date,DTE_LIC,TABLE_LICE_FACT,Date of the provider's license.,TIMESTAMP()
 Finance,Finance Capitation Dimension,Medicare Indicator,CDE_TYPE,TABLE_FIN_CAP,A code representing if recipient covered by Medicare,Char(1)
